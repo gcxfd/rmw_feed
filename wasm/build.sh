2626799d8f2de6f9710a860f09a73831e09033e1
gcxfd <i@rmw.link>
2022-06-20 18:18:19 +0800

    🔶

---
+++ wasm/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+DIR=$(cd "$(dirname "$0")"; pwd)
+cd $DIR
+set -ex
+
+if ! [ -x "$(command -v wasm-bindgen)" ]; then
+cargo install -f wasm-bindgen-cli
+fi
+
+cargo build --target wasm32-unknown-unknown --release
+
+NAME=wasm
+
+rust_wasm=../target/wasm32-unknown-unknown/release/$NAME.wasm
+
+wasm-opt --enable-reference-types -O3 -o $rust_wasm $rust_wasm
+
+wasm-bindgen \
+  $rust_wasm \
+  --out-dir pkg \
+  --target web \
+  --weak-refs \
+  --reference-types
+
+wasm-opt --enable-reference-types -O3 -o ./pkg/api_bg.wasm ./pkg/api_bg.wasm
+# wasm-opt --enable-reference-types -O3 -o ./pkg/$NAME_bg.wasm ./pkg/$NAME_bg.wasm
---
+++ wasm/src/lib.rs
@@ -0,0 +1,69 @@
+use anyhow::Result;
+use rmwlib::api::req::{Ping, Req};
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[cfg(feature = "panic_hook")]
+#[wasm_bindgen]
+pub fn prepare() {
+  std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+}
+
+macro_rules! rt {
+  ($cls:ident$fn:tt) => {{
+    let run = move || -> Result<_> { Ok(Req::$cls($cls $fn).dump()?) };
+    match run() {
+      Ok(val) => Ok(val),
+      Err(err) => Err(JsValue::from_str(&err.to_string())),
+    }
+  }};
+}
+
+type Bytes = Result<Box<[u8]>, JsValue>;
+
+#[wasm_bindgen]
+pub fn get(addr: &str, path: Box<[u8]>) -> Bytes {
+  rt!(Ping {
+    addr: addr.parse()?,
+    path,
+  })
+}
+
+/*
+*/
+
+/*
+#[wasm_bindgen]
+extern "C" {
+fn alert(s: &str);
+}
+*/
+
+/*
+#[wasm_bindgen]
+pub fn err() {
+let mut x = vec![];
+x.push(1);
+x.get(2).unwrap();
+}
+
+#[wasm_bindgen]
+pub fn hi() -> Vec<u8> {
+let original = Struct {
+number: 0x12345678ABCDEF00,
+string: "A totally pointless string".to_owned(),
+vector: vec![1, 2, 3],
+cow: Cow::Borrowed(&[4, 5, 6]),
+float: 3.1415,
+enumeration: Enum::C,
+};
+
+original.write_to_vec().unwrap()
+//let deserialized: Struct = Struct::read_from_buffer(&bytes).unwrap();
+}
+*/
