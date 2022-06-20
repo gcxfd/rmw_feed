d10b85609b268708f23067e4e97285eb2051629b
gcxfd <i@rmw.link>
2022-06-20 19:07:45 +0800

    🔶

---
+++ api/src/lib.rs
@@ -0,0 +1,24 @@
+use speedy::{Error, Readable, Writable};
+
+#[derive(PartialEq, Debug, Readable, Writable)]
+#[repr(u8)]
+#[speedy(tag_type=u8)]
+pub enum Api {
+    Stop,
+}
+
+impl Api {
+    pub fn dump(&self) -> Result<Box<[u8]>, Error> {
+        self.write_to_box()
+    }
+
+    pub fn load(bin: &[u8]) -> Result<Self, Error> {
+        Self::read_from_buffer(bin)
+    }
+
+    /*
+    pub fn on(bin: &[u8], api: &Api) -> BoxResult {
+      Self::load(bin)?.on(api)
+    }
+    */
+}
