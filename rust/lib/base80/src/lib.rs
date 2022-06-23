6a6dd0b7fd97768e6fdceafc1130e9521846b4c8
gcxfd <i@rmw.link>
2022-06-23 18:21:47 +0800

    🔶

--- rust/lib/base80/src/lib.rs
+++ rust/lib/base80/src/lib.rs
@@ -1,14 +1,12 @@
-pub fn add(left: usize, right: usize) -> usize {
-  left + right
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn it_works() {
-    let result = add(2, 2);
-    assert_eq!(result, 4);
-  }
-}
+pub const CHAR:[u8:80] = [
+  '!', '$', '(', ')', '+', ',', '-', '0',
+  '1', '2', '3', '4', '5', '6', '7', '8',
+  '9', ';', '=', '@', 'A', 'B', 'C', 'D',
+  'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+  'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+  'U', 'V', 'W', 'X', 'Y', 'Z', '[', ']',
+  '^', '_', '`', 'a', 'b', 'c', 'd', 'e',
+  'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+  'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+  'v', 'w', 'x', 'y', 'z', '{', '}', '~',
+];
