67fcb06e944d40c1a7b543fa975a52c6d1d70cec
gcxfd <i@rmw.link>
2022-06-28 12:17:37 +0800

    🔶

--- rust/net/src/udp.rs
+++ rust/net/src/udp.rs
@@ -1,3 +1,16 @@
-use std::net::SocketAddr;
+use std::net::{SocketAddr, UdpSocket};
 
-pub fn udp(addr: SocketAddr) {}
+use crate::var::MTU;
+
+pub fn udp(addr: SocketAddr) {
+  loop {
+    if let Ok(udp) = err::ok!(UdpSocket::bind(addr)) {
+      let mut buf = [0; MTU];
+      if let Ok((n, src)) = err::ok!(udp.recv_from(&mut buf)) {
+        if n <= MTU {
+          dbg!(n, src);
+        }
+      }
+    }
+  }
+}
