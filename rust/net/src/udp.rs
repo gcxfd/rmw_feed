0c3b6e764a50dc054e4954627e96d09924981519
gcxfd <i@rmw.link>
2022-06-28 17:09:05 +0800

    🔶

--- rust/net/src/udp.rs
+++ rust/net/src/udp.rs
@@ -12,22 +12,20 @@ pub fn udp(addr: SocketAddr, token: [u8; 32]) {
       let mut buf = [0; MTU];
       if let Ok((n, src)) = err::ok!(udp.recv_from(&mut buf)) {
         dbg!(src);
-        if n <= MTU {
-          if n >= 4 {
-            let id = u32::from_le_bytes(buf[..4].try_into().unwrap());
-            if id == 0 {
-              if n == 36 {
-                // stop
-                if buf[4..n] == token {
-                  return;
-                }
+        if (4..=MTU).contains(&n) {
+          let id = u32::from_le_bytes(buf[..4].try_into().unwrap());
+          if id == 0 {
+            if n == 36 {
+              // stop
+              if buf[4..n] == token {
+                return;
               }
-            } else if id % 2 == 0 { // 请求
-            } else { // 响应
             }
-            // 偶数是请求 奇数是响应 ; 请求 x 响应 x+1
-            //dbg!((n, src, &buf[..n]));
+          } else if id % 2 == 0 { // 请求
+          } else { // 响应
           }
+          // 偶数是请求 奇数是响应 ; 请求 x 响应 x+1
+          //dbg!((n, src, &buf[..n]));
         }
       }
     }
