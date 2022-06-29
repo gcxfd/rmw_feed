a57ff282ca0b32fbfb428e2804603fb7b8b30c5a
gcxfd <i@rmw.link>
2022-06-29 16:53:35 +0800

    🔶

--- rust/file/src/lib.rs
+++ rust/file/src/lib.rs
@@ -1,7 +1,8 @@
 use async_std::fs::File;
 use std::collections::BTreeMap;
+use roaring::treemap::RoaringTreemap;
 
 pub struct File {
   fs: File,
-  buf: BTreeMap<u32, Box<[u8]>>,
+  buf: BTreeMap<u32, (,Box<[u8])>>,
 }
