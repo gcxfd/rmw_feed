35ae6d8481e380c8627586eb7084bae9b93a58e9
gcxfd <i@rmw.link>
2022-06-29 13:35:31 +0800

    🔶

--- rust/lib/time/examples/main.rs
+++
@@ -1,6 +0,0 @@
-fn main() {
-  let address = "0.pool.ntp.org:123";
-  let response: ntp::packet::Packet = ntp::request(address).unwrap();
-  let ntp_time = response.transmit_time;
-  println!("{}", ntp_time);
-}
