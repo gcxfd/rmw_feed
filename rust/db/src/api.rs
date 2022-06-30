482328650751bc70c4f8e9bf2a3884dda008ca49
gcxfd <i@rmw.link>
2022-06-30 11:41:04 +0800

    .

--- rust/db/src/api.rs
+++ rust/db/src/api.rs
@@ -1,4 +1,10 @@
+use crate::{util::pk_sk, Db};
 use rkv::{column_family, Kv};
+use std::{
+  path::PathBuf,
+  sync::atomic::{AtomicU64, Ordering::Relaxed},
+};
+use util::kv::Kv as _Kv;
 
 column_family!(
   // 自增主键
@@ -12,3 +18,50 @@ column_family!(
   room_id_sk,
   room_id_name
 );
+
+macro_rules! id {
+  ($self:ident, $name:ident) => {
+    $self.$name.fetch_add(1, Relaxed).to_be_bytes()
+  };
+}
+
+pub const LOGIN: &[u8] = b"login";
+impl Db {
+  pub fn room_new<'a>(&self, name: impl AsRef<&'a str>) {
+    let id = id!(self, room_id);
+    let (pk, sk) = pk_sk();
+    let kv = &self.kv;
+    let cf = &kv.cf;
+    kv.with_tx(|tx| {
+      tx.put_cf(&cf.room_pk_id, pk, id)?;
+      tx.put_cf(&cf.room_id_sk, id, sk)?;
+      tx.put_cf(&cf.room_id_name, id, name.as_ref())?;
+      Ok(())
+    });
+  }
+
+  pub fn user_name(&self) -> Option<String> {
+    let kv = &self.kv;
+    let cf = &kv.cf;
+    if let Some(id) = kv.get(LOGIN) {
+      if let Ok(Some(name)) = err::ok!(kv.db.get_cf(&cf.user_id_name, id)) {
+        return Some(unsafe { String::from_utf8_unchecked(name) });
+      }
+    }
+    None
+  }
+
+  pub fn user_new<'a>(&self, name: impl AsRef<&'a str>) {
+    let id = id!(self, user_id);
+    let (pk, sk) = pk_sk();
+    let kv = &self.kv;
+    let cf = &kv.cf;
+    kv.with_tx(|tx| {
+      tx.put_cf(&cf.user_pk_id, pk, id)?;
+      tx.put_cf(&cf.user_id_sk, id, sk)?;
+      tx.put_cf(&cf.user_id_name, id, name.as_ref())?;
+      tx.put(LOGIN, id)?;
+      Ok(())
+    });
+  }
+}
--- rust/db/src/lib.rs
+++ rust/db/src/lib.rs
@@ -1,14 +1,12 @@
 mod api;
+mod util;
 
 pub use api::{Cf, CF_N};
-use ed25519_dalek_blake3::Keypair;
-use rand::rngs::OsRng;
-use rkv::{column_family, Kv};
+use rkv::Kv;
 use std::{
   path::PathBuf,
   sync::atomic::{AtomicU64, Ordering::Relaxed},
 };
-use util::kv::Kv as _Kv;
 
 pub use rkv::get_or_create;
 
@@ -19,60 +17,7 @@ pub struct Db {
   pub kv: Kv<Cf, CF_N>,
 }
 
-pub const LOGIN: &[u8] = b"login";
-
-pub fn pk_sk() -> ([u8; 32], [u8; 32]) {
-  let pair = Keypair::generate(&mut OsRng {});
-  let pk = pair.public.as_bytes();
-  let sk = pair.secret.as_bytes();
-  (*pk, *sk)
-}
-
-macro_rules! id {
-  ($self:ident, $name:ident) => {
-    $self.$name.fetch_add(1, Relaxed).to_be_bytes()
-  };
-}
-
 impl Db {
-  pub fn room_new<'a>(&self, name: impl AsRef<&'a str>) {
-    let id = id!(self, room_id);
-    let (pk, sk) = pk_sk();
-    let kv = &self.kv;
-    let cf = &kv.cf;
-    kv.with_tx(|tx| {
-      tx.put_cf(&cf.room_pk_id, pk, id)?;
-      tx.put_cf(&cf.room_id_sk, id, sk)?;
-      tx.put_cf(&cf.room_id_name, id, name.as_ref())?;
-      Ok(())
-    });
-  }
-
-  pub fn user_name(&self) -> Option<String> {
-    let kv = &self.kv;
-    let cf = &kv.cf;
-    if let Some(id) = kv.get(LOGIN) {
-      if let Ok(Some(name)) = err::ok!(kv.db.get_cf(&cf.user_id_name, id)) {
-        return Some(unsafe { String::from_utf8_unchecked(name) });
-      }
-    }
-    None
-  }
-
-  pub fn user_new<'a>(&self, name: impl AsRef<&'a str>) {
-    let id = id!(self, user_id);
-    let (pk, sk) = pk_sk();
-    let kv = &self.kv;
-    let cf = &kv.cf;
-    kv.with_tx(|tx| {
-      tx.put_cf(&cf.user_pk_id, pk, id)?;
-      tx.put_cf(&cf.user_id_sk, id, sk)?;
-      tx.put_cf(&cf.user_id_name, id, name.as_ref())?;
-      tx.put(LOGIN, id)?;
-      Ok(())
-    });
-  }
-
   pub fn new(path: PathBuf) -> Self {
     let kv: Kv<Cf, CF_N> = Kv::new(path);
     let cf = &kv.cf;
