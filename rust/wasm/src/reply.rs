d6e79c1f7497d5967eb6f0ba587aa39eda80d857
gcxfd <i@rmw.link>
2022-06-30 13:55:09 +0800

    .

--- rust/wasm/src/reply.rs
+++ rust/wasm/src/reply.rs
@@ -1,7 +1,7 @@
 use api::{Cmd, Reply, A, Q};
 use wasm_bindgen::prelude::*;
 
-pub fn reply(r: Reply) -> Result<JsValue> {
+pub fn reply(r: Reply) -> Result<JsValue, JsValue> {
   match r {
     Reply::None => Ok(JsValue::undefined()),
     Reply::OptionString(r) => Ok(r.into()),
