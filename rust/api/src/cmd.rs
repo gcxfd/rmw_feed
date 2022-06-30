2a8bc289b010e8f67ebdd67d2d0d3c0d00f4a44c
gcxfd <i@rmw.link>
2022-06-30 11:18:13 +0800

    🔶

---
+++ rust/api/src/cmd.rs
@@ -0,0 +1,17 @@
+use speedy::{Readable, Writable};
+
+#[derive(PartialEq, Eq, Debug, Readable, Writable, Clone)]
+#[repr(u16)]
+#[speedy(tag_type=u16)]
+pub enum Cmd {
+  Stop,
+  UserNew(String),
+  UserName,
+  RoomNew(String),
+}
+
+#[derive(PartialEq, Eq, Debug, Readable, Writable)]
+pub enum Reply {
+  Err(String),
+  None,
+}
--- rust/api/src/lib.rs
+++ rust/api/src/lib.rs
@@ -1,27 +1,12 @@
+mod cmd;
+pub use cmd::{Cmd, Reply};
 use speedy::{Readable, Writable};
-
-#[derive(PartialEq, Eq, Debug, Readable, Writable, Clone)]
-#[repr(u16)]
-#[speedy(tag_type=u16)]
-pub enum Cmd {
-  Stop,
-  UserNew(String),
-  UserName,
-  RoomNew(String),
-}
 
 #[derive(PartialEq, Eq, Debug, Readable, Writable)]
 pub struct Q {
   pub id: u32,
   pub cmd: Cmd,
 }
-
-#[derive(PartialEq, Eq, Debug, Readable, Writable)]
-pub enum Reply {
-  Err(String),
-  None,
-}
-
 #[derive(PartialEq, Eq, Debug, Readable, Writable)]
 pub struct A {
   pub id: u32,
