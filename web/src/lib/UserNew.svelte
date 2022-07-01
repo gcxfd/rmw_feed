366e2c085fba0f8aea3e5f926f5e34fe25f1aa33
gcxfd <i@rmw.link>
2022-07-01 17:51:10 +0800

    🔶

--- web/src/lib/UserNew.svelte
+++ web/src/lib/UserNew.svelte
@@ -1,10 +1,24 @@
 <script lang="coffee">
+import { onMount } from 'svelte'
+
+:$ input
+
 :$ name = ''
+
+:$ submit = =>
+  input.focus()
+  console.log name
+  return
+
+onMount =>
+  input.focus()
+  return
+
 </script>
 
 <template lang="pug">
-form
-  input(bind:value="{ name }" placeholder="请输入姓名")
+form(on:submit|preventDefault="{ submit }")
+  input(bind:this="{ input }" bind:value="{ name }" placeholder="请输入姓名")
   button(type="submit") 创建用户
 </template>
 
