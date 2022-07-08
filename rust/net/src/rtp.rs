b70cc8f06627c8ae842f6210bbb8b431a8db28a4
gcxfd <i@rmw.link>
2022-07-08 17:35:02 +0800

    🔶

--- rust/net/src/rtp.rs
+++ rust/net/src/rtp.rs
@@ -1,3 +1,5 @@
+// rmw transport transport
+
 use std::collections::BTreeMap;
 
 pub struct Node {
@@ -10,9 +12,16 @@ pub struct Bt<Addr: ToSocketAddrs> {
   ping_period: u64,
   min_delay: u16,
   max_delay: u16,
-  node: BTreeMap<u64, Node>,
+  queue: BTreeMap<u64, Node>,
 }
 
 impl Bt {
   fn pong(&mut self) {}
+
+  fn run(&mut self) {
+    loop {
+      let now = time::ms();
+      for (time, li) in self.queue.range_mut(..=now) {}
+    }
+  }
 }
