19ccdcc6d0a70307fe009b9127db31c2c53d8352
gcxfd <i@rmw.link>
2022-07-08 11:43:28 +0800

    🔶

--- rust/db/src/lib.rs
+++ rust/db/src/lib.rs
@@ -3,42 +3,51 @@ mod cf;
 use std::{path::PathBuf, sync::atomic::AtomicU64};
 
 pub use cf::{Cf, CF_N};
+use paste::paste;
 use rkv::Kv;
 
-#[derive(Debug)]
-pub struct Db {
-  pub user_id: AtomicU64,
-  pub room_id: AtomicU64,
-  pub kv: Kv<Cf, CF_N>,
-}
-
-impl Db {
-  pub fn new(path: PathBuf) -> Self {
-    let kv: Kv<Cf, CF_N> = Kv::new(path);
-    let cf = &kv.cf;
-
-    macro_rules! init_id {
-      ($key:expr) => {{
-        let key_str = stringify!($key);
-        AtomicU64::new(
-          kv.with_tx(|tx| {
-            Ok(if let Some(id) = tx.get_cf(&cf.id, key_str)? {
-              u64::from_le_bytes((&id[..8]).try_into()?)
-            } else {
-              let id = 0u64;
-              tx.put_cf(&cf.id, key_str, id.to_le_bytes())?;
-              id
-            })
-          })
-          .unwrap(),
-        )
-      }};
+macro_rules! db {
+  ($($table:ident),*) => {
+    paste! {
+    #[derive(Debug)]
+    pub struct Db {
+      $(pub [<$table _id>]: AtomicU64,)*
+      pub kv: Kv<Cf, CF_N>,
+    }
     }
 
-    Self {
-      user_id: init_id!(user),
-      room_id: init_id!(room),
-      kv,
+    impl Db {
+      pub fn new(path: PathBuf) -> Self {
+        let kv: Kv<Cf, CF_N> = Kv::new(path);
+        let cf = &kv.cf;
+
+        macro_rules! init_id {
+          ($key:expr) => {{
+            let key_str = stringify!($key);
+            AtomicU64::new(
+              kv.with_tx(|tx| {
+                Ok(if let Some(id) = tx.get_cf(&cf.id, key_str)? {
+                  u64::from_le_bytes((&id[..8]).try_into()?)
+                } else {
+                  let id = 0u64;
+                  tx.put_cf(&cf.id, key_str, id.to_le_bytes())?;
+                  id
+                })
+              })
+              .unwrap(),
+            )
+          }};
+        }
+
+        paste! {
+          Self {
+            $([<$table _id>]: init_id!($table),)*
+            kv,
+          }
+        }
+      }
     }
-  }
+  };
 }
+
+db!(user, room);
