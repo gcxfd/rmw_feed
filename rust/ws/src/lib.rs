06c2a7a616b6a4f54d25c77b096cb3d4a0ff2e6a
gcxfd <i@rmw.link>
2022-07-08 14:08:30 +0800

    🔶

--- rust/ws/src/lib.rs
+++ rust/ws/src/lib.rs
@@ -31,9 +31,7 @@ pub fn run(run: &Run, api: &Arc<Api>) {
           err::log!(tcp::ws(stream, api).await);
         });
       }
-      dbg!("tcp done");
     } else {
-      dbg!("tcp err");
       err::log!(cmd(&api, Cmd::Stop).await);
     }
   });
