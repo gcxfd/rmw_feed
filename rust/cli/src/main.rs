72eca5993872d58dc809f2dd015b4e72e6a63a17
gcxfd <i@rmw.link>
2022-07-08 14:44:24 +0800

    🔶

--- rust/cli/src/main.rs
+++ rust/cli/src/main.rs
@@ -4,21 +4,23 @@ use speedy::{Readable, Writable};
 use tungstenite::{connect, Message, Message::Binary};
 
 fn main() -> Result<()> {
-  let (mut socket, response) = connect("ws://localhost:3012").expect("Can't connect");
+  logger::init().apply()?;
 
-  let q = Q {
-    id: 0,
-    cmd: Cmd::UserNew("test".to_string()),
-  };
+  if let Ok((mut socket, _)) = err::ok!(connect("ws://127.0.0.1:4910")) {
+    let q = Q {
+      id: 0,
+      cmd: Cmd::UserNew("test".to_string()),
+    };
 
-  socket.write_message(Message::Binary(q.dump()?.into()))?;
+    socket.write_message(Message::Binary(q.dump()?.into()))?;
 
-  match socket.read_message()? {
-    Binary(bin) => {
-      let a = A::load(&bin);
-      println!("Received: {:?}", a);
+    match socket.read_message()? {
+      Binary(bin) => {
+        let a = A::load(&bin);
+        println!("Received: {:?}", a);
+      }
+      _ => {}
     }
-    _ => {}
   }
   Ok(())
 }
