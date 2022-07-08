bbb5a6f704763cc4f6bfe9d70ce14d77c0c9d85c
gcxfd <i@rmw.link>
2022-07-08 14:58:27 +0800

    🔶

--- rust/logger/src/lib.rs
+++ rust/logger/src/lib.rs
@@ -6,16 +6,16 @@ pub fn init() -> fern::Dispatch {
       let line = record.line().unwrap_or(0);
       let level = record.level();
       let tip = (format_args!(
-        "{} {} {}:{}{}",
-        chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+        "{} {}{} {}",
         level,
-        record.target(),
+        //record.target(),
         record.file().unwrap_or(""),
         if line > 0 {
           format!(":{}", line)
         } else {
           "".to_string()
-        }
+        },
+        chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
       ))
       .to_string();
       {
