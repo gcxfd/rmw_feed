b5594737d8961ebe5751887590f582e2fe72c3fa
gcxfd <i@rmw.link>
2022-07-14 15:28:44 +0800

    🔶

--- rust/net/src/udp.rs
+++ rust/net/src/udp.rs
@@ -24,8 +24,8 @@ impl Udp {
     loop {
       if let Ok((n, src)) = err::ok!(await_udp.recv_from(&mut buf).await) {
         dbg!(src);
-        if n > 4 {
-          let id = u32::from_le_bytes(buf[..4].try_into().unwrap());
+        if n > 8 {
+          let id = u64::from_le_bytes(buf[..8].try_into().unwrap());
           if id % 2 == 0 { // 请求
              // 偶数是请求 奇数是响应 ; 请求 x 响应 x+1
           } else { // 响应
